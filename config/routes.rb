6f9648bf9354a9444da7cf797594f0a3071bc817
stephane brossier <sbrossier@groupon.com>
2015-02-02 14:41:24 -0800

    Remove unused tests resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Kanaui::Engine.routes.draw do
 
-  resources :tests, :only => [ :show, :index ]
-
   resources :dashboard, :only => [ :index ]
   scope "/dashboard" do
     match "/available_reports" => "dashboard#available_reports", :via => :get, :as => "available_reports"
