b8c9e0a18a0e400bd7ac3ae869c3c04632790c62
stephane brossier <sbrossier@groupon.com>
2015-02-12 19:46:22 -0800

    Cleanup + Gemfile.lock

--- test/dummy/config/initializers/killbill_client.rb
+++ test/dummy/config/initializers/killbill_client.rb
@@ -1,4 +1,4 @@
 KillBillClient.url = 'http://127.0.0.1:8080/'
-KillBillClient.api_key = 'mos'
-KillBillClient.api_secret = 'mos'
+KillBillClient.api_key = 'bob'
+KillBillClient.api_secret = 'lazar'
 
