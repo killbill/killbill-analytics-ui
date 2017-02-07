6c1800eb0ea8331dc86ac959a25e31901fb7a96e
Pierre-Alexandre Meyer <pierre@mouraf.org>
2017-02-07 01:49:03 -0800

    charts: prettify axes
    * Re-order elements so axes are always on top
    * Don't display vertical text on Y-axis (same as report name, doesn't give much)
    
    Signed-off-by: Pierre-Alexandre Meyer <pierre@mouraf.org>

--- app/assets/javascripts/kanaui/kiddo/axes.js
+++ app/assets/javascripts/kanaui/kiddo/axes.js
@@ -23,32 +23,32 @@
       x: xAxis,
       y: yAxis,
       render: function(svg, yTitle){
-        svg.append("g")
-          .attr("class", "x axis")
-          .attr("transform", "translate(" + self.margin_left + "," + self.height + ")")
-          .call(xAxis);
-
-        svg.append("g")
-          .attr("class", "y axis")
-          .attr("transform", "translate(" + self.margin_left + ",0)")
-          .call(yAxis)
-
-        .append("text")
-          .attr("transform", "rotate(-90)")
-          .attr("y", 6)
-          .attr("dy", ".71em")
-          .style("text-anchor", "end")
-          .text(yTitle);
+          svg.append("g")
+              .attr("class", "grid")
+              .attr("transform", "translate(" + self.margin_left + "," + self.height + ")")
+              .call(makeXAxis().tickSize(-self.height, 0, 0).tickFormat(""));
 
           svg.append("g")
-            .attr("class", "grid")
-            .attr("transform", "translate(" + self.margin_left + "," + self.height + ")")
-            .call(makeXAxis().tickSize(-self.height, 0, 0).tickFormat(""));
+              .attr("class", "grid")
+              .attr("transform", "translate(" + self.margin_left + ",0)")
+              .call(makeYAxis().tickSize(-self.width, 0, 0).tickFormat(""));
 
-           svg.append("g")
-             .attr("class", "grid")
-             .attr("transform", "translate(" + self.margin_left + ",0)")
-             .call(makeYAxis().tickSize(-self.width, 0, 0).tickFormat(""));
+          svg.append("g")
+              .attr("class", "x axis")
+              .attr("transform", "translate(" + self.margin_left + "," + self.height + ")")
+              .call(xAxis);
+
+          svg.append("g")
+              .attr("class", "y axis")
+              .attr("transform", "translate(" + self.margin_left + ",0)")
+              .call(yAxis);
+              //.append("text")
+              //.attr("transform", "rotate(-90)")
+              //.attr("y", 6)
+              //.attr("dy", ".71em")
+              //.style("text-anchor", "end")
+              //.text(yTitle);
+
       }
     };
   };
