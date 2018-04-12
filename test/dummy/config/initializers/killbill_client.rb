6766ff9136e591ab3e14fe56f1efdf9fba52e327
Pierre-Alexandre Meyer <pierre@mouraf.org>
2018-04-12 08:46:07 -0700

    config: make test app configurable through environment variables
    Signed-off-by: Pierre-Alexandre Meyer <pierre@mouraf.org>

--- test/dummy/config/initializers/killbill_client.rb
+++ test/dummy/config/initializers/killbill_client.rb
@@ -1,3 +1,3 @@
-KillBillClient.url = 'http://127.0.0.1:8080/'
-KillBillClient.api_key = 'bob'
-KillBillClient.api_secret = 'lazar'
+KillBillClient.url = ENV['KILLBILL_URL'] || 'http://127.0.0.1:8080/'
+KillBillClient.api_key = ENV['KILLBILL_API_KEY'] || 'bob'
+KillBillClient.api_secret = ENV['KILLBILL_API_SECRET'] || 'lazar'
